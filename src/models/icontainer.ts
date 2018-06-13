8808ff1dbd05979ec4fbf7418db8dde83507f7e7
Alexander <alex.c.hayes08@gmail.com>
2018-06-13 15:04:01 -0500

    First commit for the angular version.

--- src/models/icontainer.ts
+++
@@ -1,5 +0,0 @@
-import * as d3 from "d3";
-
-export interface IContainer {
-    container: d3.Selection<SVGGElement, {}, null, undefined>;
-}
--- src/models/ioperation-callback.ts
+++
@@ -1,5 +0,0 @@
-export interface IOperationCallbacks<T> {
-    onBefore?: (context: T) => void;
-    onDuring?: (context: T) => void;
-    onAfter?: (context: T) => void;
-}

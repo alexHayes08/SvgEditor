fb644277ec585dc0093ac4d6e2b3976274f7e566
Alexander <alex.c.hayes08@gmail.com>
2018-06-13 15:43:24 -0500

    Latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SvgDefsComponent } from './svg-defs/svg-defs.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //#region Fields
+
+  public title = 'SvgEditor';
+
+  @ViewChild('.editableArea')
+  private _editableAreaEl!: SVGClipPathElement;
+
+  @ViewChild(".handles")
+  private _handlesEl!: SVGGElement;
+  
+  @ViewChild("defs")
+  private _defsEl!: SVGDefsElement;
+  // public defs: SvgDefsComponent;
+
+  //#endregion
+
+  //#region Constructor
+
+  constructor() { }
+
+  //#endregion
+
+  //#region Properties
+
+  //#endregion
+
+  //#region Functions
+
+  //#endregion
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SvgDefsComponent } from './svg-defs/svg-defs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SvgDefsComponent
   ],
   imports: [
     BrowserModule
